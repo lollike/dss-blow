bd7618b3bec7cebc051b0885bc6266a68f75ef59
Lollike <nikolajzl@hotmail.com>
2021-11-07 14:47:47 +0100

    fuzzing test that blow works

--- src/DssBlow.t.sol
+++ src/DssBlow.t.sol
@@ -12,6 +12,73 @@ interface Hevm {
 }
 
 contract DssBlowTest is DSTest {
+    // --- MATH ---
+    function add(uint x, uint y) internal pure returns (uint z) {
+        require((z = x + y) >= x, "ds-math-add-overflow");
+    }
+    function sub(uint x, uint y) internal pure returns (uint z) {
+        require((z = x - y) <= x, "ds-math-sub-underflow");
+    }
+    function mul(uint x, uint y) internal pure returns (uint z) {
+        require(y == 0 || (z = x * y) / y == x, "ds-math-mul-overflow");
+    }
+
+    function min(uint x, uint y) internal pure returns (uint z) {
+        return x <= y ? x : y;
+    }
+    function max(uint x, uint y) internal pure returns (uint z) {
+        return x >= y ? x : y;
+    }
+    function imin(int x, int y) internal pure returns (int z) {
+        return x <= y ? x : y;
+    }
+    function imax(int x, int y) internal pure returns (int z) {
+        return x >= y ? x : y;
+    }
+
+    //rounds to zero if x*y < WAD / 2
+    function wmul(uint x, uint y) internal pure returns (uint z) {
+        z = add(mul(x, y), WAD / 2) / WAD;
+    }
+    //rounds to zero if x*y < WAD / 2
+    function rmul(uint x, uint y) internal pure returns (uint z) {
+        z = add(mul(x, y), RAY / 2) / RAY;
+    }
+    //rounds to zero if x*y < WAD / 2
+    function wdiv(uint x, uint y) internal pure returns (uint z) {
+        z = add(mul(x, WAD), y / 2) / y;
+    }
+    //rounds to zero if x*y < RAY / 2
+    function rdiv(uint x, uint y) internal pure returns (uint z) {
+        z = add(mul(x, RAY), y / 2) / y;
+    }
+
+    // This famous algorithm is called "exponentiation by squaring"
+    // and calculates x^n with x as fixed-point and n as regular unsigned.
+    //
+    // It's O(log n), instead of O(n) for naive repeated multiplication.
+    //
+    // These facts are why it works:
+    //
+    //  If n is even, then x^n = (x^2)^(n/2).
+    //  If n is odd,  then x^n = x * x^(n-1),
+    //   and applying the equation for even x gives
+    //    x^n = x * (x^2)^((n-1) / 2).
+    //
+    //  Also, EVM division is flooring and
+    //    floor[(n-1) / 2] = floor[n / 2].
+    //
+    function rpow(uint x, uint n) internal pure returns (uint z) {
+        z = n % 2 != 0 ? x : RAY;
+
+        for (n /= 2; n != 0; n /= 2) {
+            x = rmul(x, x);
+
+            if (n % 2 != 0) {
+                z = rmul(z, x);
+            }
+        }
+    }
 
     uint256 constant WAD = 10 ** 18;
     uint256 constant RAY = 10 ** 27;
@@ -26,7 +93,6 @@ contract DssBlowTest is DSTest {
     address daiJoin = 0x9759A6Ac90977b93B58547b4A71c78317f391A28;
     address dai_ = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
 
-
     function setUp() public {
         hevm = Hevm(address(bytes20(uint160(uint256(keccak256('hevm cheat code'))))));
         blow = new DssBlow(daiJoin, vow);
@@ -34,26 +100,19 @@ contract DssBlowTest is DSTest {
         vat = VatAbstract(0x35D1b3F3D7966A1DFe207aa4514C12a259A0492B);
     }
 
-    function testFail_basic_sanity() public {
-        assertTrue(false);
-    }
-
-    function test_basic_sanity() public {
-        assertTrue(true);
-    }
-
-    function test_blow_contract_balance() public {
-        uint256 amount = 10 * WAD;
-        assertEq(dai.balanceOf(address(this)), 0);
+    function test_blow_contract_balance(uint64 amount) public {
+        assertEq(dai.balanceOf(address(blow)), 0);
         uint256 vowBalance = vat.dai(vow);
+        
         _giveTokens(dai, amount);
         dai.approve(address(blow), amount);
         dai.transferFrom(address(this), address(blow), amount);
         assertEq(dai.balanceOf(address(blow)), amount);
+        
         blow.blow();
+        
         assertEq(dai.balanceOf(address(blow)), 0);
-        //assertEq(vat.dai(vow), vowBalance+amount);
-
+        assertEq(vat.dai(vow), add(vowBalance, amount*RAY));
     }
 
     function _giveTokens(DSTokenAbstract token, uint256 amount) internal {
